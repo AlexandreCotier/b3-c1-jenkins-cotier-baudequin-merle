67e70ed43d873242706ec5d8c301429ddd0e7baa
alexandrecotier <alexandre.cotier@icloud.com>
2022-12-18 22:07:07 +0100

    Tests implemented

---
+++ test/app.test.js
@@ -0,0 +1,58 @@
+const Calculator = require('../src/calculator').Calculator;
+const expect = require('chai').expect;
+
+describe('Testing the Calculator Functions', function() {
+    it('1. Sum', function(done) {
+        expect(Calculator.sum(4,2)).to.equal(6);
+        done();
+    });
+
+    it('2. Multiply', function(done) {
+        expect(Calculator.multiply(2,4)).to.equal(8);
+        done();
+    });
+
+    it('3. Substract', function(done) {
+        expect(Calculator.substract(6,4)).to.equal(2);
+        done();
+    });
+
+    it('4. Division', function(done) {
+        expect(Calculator.division(6,2)).to.equal(3);
+        done();
+    });
+
+    it('7. Percentil', function(done){
+        expect(Calculator.percentil(10,100)).to.equal(10);
+        done();
+    })
+    it('8. Sum float', function(done) {
+        expect(Calculator.sum(4.8,2.5)).to.equal(7.3);
+        done();
+    });
+
+    it('9. Multiply float', function(done) {
+        expect(Calculator.multiply(2,4.3)).to.equal(8.6);
+        done();
+    });
+
+    it('10. Division float', function(done) {
+        expect(Calculator.division(145.2,12.1)).to.equal(12);
+        done();
+    });
+
+    it('11. Division float result', function(done) {
+        expect(Calculator.division(10,3)).to.equal(3.3333333333333335);
+        done();
+    });
+
+    it('12. Division zero', function(done) {
+        expect(Calculator.division(10,0)).to.equal(Infinity);
+        done();
+    });
+
+    it('13. lettre', function(done) {
+        expect(Calculator.division(10,'a')).to.equal('');
+        done();
+    }); // j'ai voulu faire un test pour quand ya pas un chiffre
+});
